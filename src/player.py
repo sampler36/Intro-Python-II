6f8f7e9b0f8b948519ddd7330ecda73c5ab990c8
Talent Antonio <antonio.talenty@gmail.com>
2019-05-30 16:27:46 +0100

    main loops

--- src/player.py
+++ src/player.py
@@ -4,5 +4,5 @@ class Player:
     def __init__(self, name):
         self.name = name
 
-    def __str__(self):
-        return "" + self.name
+    # def __str__(self):
+    #     return "" + self.name
--- src/room.py
+++ src/room.py
@@ -1,21 +1,18 @@
 # Implement a class to hold room information. This should have name power ups and power downs / lights_on or lights_out
 # description attributes.
 class Room:
-    def __init__ (self, name, lights_on=True, lights_out=True):
+    def __init__ (self, name):
             # attributes / fields
        self.name = name
        self.lights_on = lights_on
        self.lights_out = lights_out
 
-    def __str__(self):
-       return f"\nName: {self.name} \nLight On: {self.lights_on} \nLight Out: {self.lights_out} \n"
+    # def __str__(self):
+    #    return self.name
 
-a = Room("Room 1", True, False)
-b = Room("Room 2", True, False)
-c = Room("Room 3", True, False)
-d = Room("Room 4", False, True)
-e = Room("Room 5", True, False)
-f = Room("Room 6", False, True)
-
-
-print(a)
+# a = Room("Room 1", True, False)
+# b = Room("Room 2", True, False)
+# c = Room("Room 3", True, False)
+# d = Room("Room 4", False, True)
+# e = Room("Room 5", True, False)
+# f = Room("Room 6", False, True)
